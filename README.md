428fb4de6bfed98e5d10e1f4dfa9cde545b5c23b
Drew Smith <drgh056@gmail.com>
2023-03-17 20:48:56 -0500

    update all files with newer versions

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# js-quiz
+# Javascript Quiz!
+
+lightspeed: https://pixabay.com/illustrations/lightspeed-explosion-pattern-blur-7290642/
+
+blackhole sun: https://pixabay.com/illustrations/abstract-astral-astrology-astronomy-2615764/
+
+
+earth: https://pixabay.com/illustrations/earth-globe-blue-planet-space-home-2078484/
+
+stars: https://pixabay.com/illustrations/stars-sky-space-space-wallpaper-2643089/
+
+tavern: https://pixabay.com/illustrations/tavern-pub-architecture-restaurant-7793838/
+
+TAVERN2: https://pixabay.com/illustrations/tavern-pub-architecture-restaurant-7793824/
+
+peasant-hand-extended: https://pixabay.com/illustrations/medieval-3d-cartoon-man-boy-7267880/
+
+peasant-stand-behind: https://pixabay.com/illustrations/medieval-3d-cartoon-man-boy-7267875/
+
+peasant sign: https://pixabay.com/illustrations/medieval-3d-cartoon-man-boy-7267877/
+
+peasant megaphone: https://pixabay.com/illustrations/medieval-3d-cartoon-man-boy-7267878/
+
+peasant train: https://pixabay.com/illustrations/man-medieval-3d-cartoon-character-7293563/
+
+peasant beside and point: https://pixabay.com/illustrations/medieval-3d-cartoon-man-boy-7267874/
+
+peasant mug: https://pixabay.com/illustrations/medieval-beer-pint-ale-alcohol-7287003/
+
+Sound Effect from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=19107">Pixabay</a>
---
+++ assets/scripts/script.js
@@ -0,0 +1,149 @@
+var questionBank = [
+
+    qOne = {
+        question: "While the word 'friend' opens the door to the mines of Moria in the Fellowship of the Ring, what language must it be spoken in?",
+        answers: ["dwarvish", "common tongue", "elvish", "black tongue"],
+        correctIndex: 2
+    },
+
+    qTwo = {
+        question: "In the Discworld series, what is Death's favorite animal?",
+        answers: ["cats", "rats", "ravens", "dogs"],
+        correctIndex: 0
+    },
+
+    qThree = {
+        question: "In Harry Potter and the Sorceror's Stone, what word backwards is the name bestowed upon a mirror that will show you what you want most?",
+        answers: ["wish", "desire", "treasure", "fortune"],
+        correctIndex: 1
+    },
+
+    qFour = {
+        question: "In the Wheel of Time series, what is the half of the power that males can wield?",
+        answers: ["Saidar", "Callandor", "Callaondir", "Saidin"],
+        correctIndex: 3
+    },
+
+    qFive = {
+        question: "What mythical group does Kvothe seek after they murdered his parents in The Name of the Wind?",
+        answers: ["Fae", "Severan", "Edema", "Chandrian"],
+        correctIndex: 3
+    },
+
+    qSix = {
+        question: "In the Sword of Truth series, Kahlan Amnell possesses the power of what magical order of women?",
+        answers: ["Bone Readers", "Mord-sith", "Confessors", "Gars"],
+        correctIndex: 2
+    },
+
+    qSeven = {
+        question: "What is the surname of the main family of children who enter Narnia in The Lion, The Witch, and the Wardrobe?",
+        answers: ["Pevensie", "Kirke", "Scrubb", "Pole"],
+        correctIndex: 0
+    },
+
+    qEight = {
+        question: "Bartimaeus, from the Bartimaeus Sequence, belongs to which class of demon?",
+        answers: ["Afrit", "Naeryan", "Jabor", "Djinn"],
+        correctIndex: 3
+    },
+
+    qNine = {
+        question: "In Redwall, young mouse monk Matthias must confront a venomous adder named what?",
+        answers: ["Clooney", "Asmodeus", "Vilu", "Tsarmina"],
+        correctIndex: 1
+    },
+
+    qTen = {
+        question: "Which house bears the motto 'We do not Sow' in the Song of Ice and Fire series?",
+        answers: ["Stark", "Greyjoy", "Lannister", "Baratheon"],
+        correctIndex: 1
+    }
+
+
+]
+
+
+
+// startQuiz() {
+
+// }
+
+// Start timer
+// Display Next question
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+var  questions = [
+    {
+        questionText: "What's your favorite color?",
+        questionChoices: ["red", "blue", "green"],
+        answer: 2
+    }
+var time = 100; 
+var timerEl = document.querySelector("#time-el")
+var questionIndex = 0;
+
+
+function displayQuestion() {
+    main.innerHTML= "";
+    var h1El = document.createElement("h1");
+    h1El.textContent = questions[questionIndex].questionText;
+    mainEl.appendChild(h1El);
+    
+    for (var i = 0; i < questions[questionIndex].questionChoice.length; i++) {
+        var buttonEl = document.createElement("button");
+        buttonEl.textContent = questions[questionIndex].questionChoices[i];
+        button add class 
+        mainEl.appendChild(buttonEl);
+    }
+    questions[questionIndex].questionChoices
+    
+    questionIndex++;
+
+}
+
+startBtnEl.addEventListener("click", function (event){
+    mainEl.innerHTML = ""
+})
+
+    interval = setInterval(function() {
+        time--; 
+        timerEl.textContent = `Time: ${time}`;
+        if (time <= 0) {
+            clearInterval(interval);
+            endGame();
+            return;
+        }
+    }, 1000);
+
+    displayQuestion();
+
+
+    mainEl.addEvenetListerner("click", function(event)) {
+        var target = event.target
+
+        if (target.getAttribute("class"))
+    }
+
+    function endGame() {
+
+    }
+
+
+*/
